7768d3fd9530894d04ab42ccb5411ee7d5f1a9ad
n-mizunuma <n-mizunuma@froide.co.jp>
2019-12-18 14:32:52 +0900

    検証用のindexにseedを構築

--- Node.js
+++ Node.js
@@ -55,138 +55,137 @@ module.exports = (async function(){
     },
   };
 
+  this.ROOT = null;
+  this.current = null;
   this.woods = async function(){};
   this.checkout = async function(name=this.ROOT_HASH){
+    const root = await DB.connect(async connection=>{
+      const Node = await DB.get_collection(`${APP.name}/nodes`);
+      return await Node.findOne({parent: "",key: name});
+    });
+    if(root){
+      this.ROOT = root;
+      this.current = this.ROOT;
+      return this.current;
+    }else{ return null; }
+  }; await this.checkout();
+  this.target = key=>{
+    if(key==this.ROOT.key)return this.ROOT;
+    return this.current;
+  };
+
+  this.dimension = async function(){}; //未実装：
+  this.cosmos = async function(){}; //未実装
+  this.universe = async function(){}; //未実装：
+  this.space = async function(){}; //未実装：
+  this.chunk = async function(){}; //未実装：
+  this.forest = async function(){}; //未実装：
+  // this.checkout = async function(name=this.ROOT_HASH){
+  //   const root_node = await this.childs(name, {hash: ""});
+  //   if(root_node.length){
+  //     this.ROOT = root_node[0];
+  //     this.current = this.ROOT;
+  //     return this.current;
+  //   }else{ return null; }
+  // };
+  this.snapshot = async function(){}; //未実装：ツリーを圧縮保存
+  this.independent = async function(){}; //未実装：親が不在のノードを独立させる
+  this.find = async function(path=""){
+    if(typeof path==="string" && path.length>0){
+      const node_keys = path.split("/");
+      let find_history = [];
+      let node = this.target(node_keys[0]);
+
+      for(k in node_keys){
+        if(node_keys[k]==this.ROOT.key){}
+        else if(node_keys[k]=='.'){}
+        else if(node_keys[k]=='..'){ node = await this.parent(node); }
+        else{
+          const result = await this.childs(node_keys[k], node);
+          if(result.length>0){ node = result[0]; }
+          else{ return find_history; }
+        }
+        find_history.unshift(node);
+      }
+      return node;
+    }else{return `find() $path type error [${typeof path}]`;}
+  };
+  // [TBI] snap serialize history undo redo diff grep
+  this.log = (node=null)=>console.log(node?node:this.current);
+  this.parent = async function(node=null){
+    if(!node)node = this.current;
+    if(!node.parent) return node;
+    return await this.one.read(node.parent);
+  };
+  this.childs = async function(key=null, node=null){
+    if(!node)node = this.current;
     return await DB.connect(async connection=>{
       const Node = await DB.get_collection(`${APP.name}/nodes`);
-      const root await Node.findOne({param: "",key: name});
-      if(root.length){
-        this.ROOT = root;
-        this.current = this.ROOT;
-        return this.current;
-      }else{ return null; }
+      let params = {parent: node.hash};
+      if(key) params["key"] = key;
+      return await Node.find(params).toArray();
     });
-  }; this.checkout();
-  this.True = ()=>!!this.ROOT;
-  if(this.True()){
-    this.current = this.ROOT;
-    this.target = key=>{
-      if(key==this.ROOT.key)return this.ROOT;
+  };
+  this.branch = async function(node=null){
+    if(!node)node = this.current;
+    let parents = [node];
+    while(true){
+      node = await this.parent(node);
+      parents.unshift(node);
+      if(node.parent=="") break;
+    }
+    return parents;
+  };
+  this.cd = async function(path=""){
+    if(typeof path==="string" && path.length>0){
+      this.current = path.length==0? this.ROOT : await this.find(path);
       return this.current;
-    };
-    this.dimension = async function(){}; //未実装：
-    this.cosmos = async function(){}; //未実装
-    this.universe = async function(){}; //未実装：
-    this.space = async function(){}; //未実装：
-    this.chunk = async function(){}; //未実装：
-    this.forest = async function(){}; //未実装：
-    this.checkout = async function(name=this.ROOT_HASH){
-      const root_node = await this.childs(name, {hash: ""});
-      if(root_node.length){
-        this.ROOT = root_node[0];
-        this.current = this.ROOT;
-        return this.current;
-      }else{ return null; }
-    };
-    this.snapshot = async function(){}; //未実装：ツリーを圧縮保存
-    this.independent = async function(){}; //未実装：親が不在のノードを独立させる
-    this.find = async function(path=""){
-      if(typeof path==="string" && path.length>0){
-        const node_keys = path.split("/");
-        let find_history = [];
-        let node = this.target(node_keys[0]);
+    }else{return `cd() $path type error [${typeof path}]`;}
+  };
+  this.ls = async function(key){ return (await this.childs(key)).map(n=>n.key); };
+  this.pwd = async function(node=null){
+    if(!node)node = this.current;
+    let keys = [node.key];
+    while(true){
+      parent_node = await this.parent(node);
+      if(node==parent_node){ break; }
+      else{ keys.unshift((node=parent_node).key); }
+    }
+    return keys.join('/');
+  };
+  this.cat = function(node=null){
+    if(!node)node = this.current;
+    return node["value"];
+  };
+  this.make = async function(path, root_flg=false, option={p:true}){
+    const node_keys = path.split("/");
+    let key="", parent_node=this.current;
+    if(node_keys.length>0) key = node_keys.pop();
+    if(node_keys.length>0) parent_node = await this.find( node_keys.join("/") );
 
-        for(k in node_keys){
-          if(node_keys[k]==this.ROOT.key){}
-          else if(node_keys[k]=='.'){}
-          else if(node_keys[k]=='..'){ node = await this.parent(node); }
-          else{
-            const result = await this.childs(node_keys[k], node);
-            if(result.length>0){ node = result[0]; }
-            else{ return find_history; }
-          }
-          find_history.unshift(node);
+    //console.log(key, node_keys);
+    if(APP.is.a(parent_node) && option["p"]){ //leafが存在しない場合
+      for([k,v] of Object.entries(node_keys)){
+        if(!parent_node[k]){ // leafまでの各種nodeが存在しない場合
+          path = node_keys.slice(0,parseInt(k)+1).join("/");
+          parent_node[k] = await this.make(path, false, {p:false});
         }
-        return node;
-      }else{return `find() $path type error [${typeof path}]`;}
-    };
-    // [TBI] snap serialize history undo redo diff grep
-    this.log = (node=null)=>console.log(node?node:this.current);
-    this.parent = async function(node=null){
-      if(!node)node = this.current;
-      if(!node.parent) return node;
-      return await this.one.read(node.parent);
-    };
-    this.childs = async function(key=null, node=null){
-      if(!node)node = this.current;
-      return await DB.connect(async connection=>{
-        const Node = await DB.get_collection(`${APP.name}/nodes`);
-        let params = {parent: node.hash};
-        if(key) params["key"] = key;
-        return await Node.find(params).toArray();
-      });
-    };
-    this.branch = async function(node=null){
-      if(!node)node = this.current;
-      let parents = [node];
-      while(true){
-        node = await this.parent(node);
-        parents.unshift(node);
-        if(node.parent=="") break;
       }
-      return parents;
-    };
-    this.cd = async function(path=""){
-      if(typeof path==="string" && path.length>0){
-        this.current = path.length==0? this.ROOT : await this.find(path);
-        return this.current;
-      }else{return `cd() $path type error [${typeof path}]`;}
-    };
-    this.ls = async function(key){ return (await this.childs(key)).map(n=>n.key); };
-    this.pwd = async function(node=null){
-      if(!node)node = this.current;
-      let keys = [node.key];
-      while(true){
-        parent_node = await this.parent(node);
-        if(node==parent_node){ break; }
-        else{ keys.unshift((node=parent_node).key); }
-      }
-      return keys.join('/');
-    };
-    this.cat = function(node=null){
-      if(!node)node = this.current;
-      return node["value"];
-    };
-    this.make = async function(path, root_flg=false, option={p:true}){
-      const node_keys = path.split("/");
-      let key="", parent_node=this.current;
-      if(node_keys.length>0) key = node_keys.pop();
-      if(node_keys.length>0) parent_node = await this.find( node_keys.join("/") );
+      parent_node = parent_node.pop();
+    }
 
-      //console.log(key, node_keys);
-      if(APP.is.a(parent_node) && option["p"]){ //leafが存在しない場合
-        for([k,v] of Object.entries(node_keys)){
-          if(!parent_node[k]){ // leafまでの各種nodeが存在しない場合
-            path = node_keys.slice(0,parseInt(k)+1).join("/");
-            parent_node[k] = await this.make(path, false, {p:false});
-          }
-        }
-        parent_node = parent_node.pop();
-      }
-
-      const parent_hash = root_flg ? "" : parent_node.hash;
-      const node_data = this.createModel({parent: parent_hash, key: key});
-      const create_node = await this.one.create(node_data);
-      //return await this.cd(await this.pwd(create_node)); // make後にcd
-      return create_node;
-    };
-    this.set = async function(node_data, node=null){
-      if(!node)node = this.current;
-      node_data.updated_at = APP.s_date();
-      await this.one.update(node_data);
-      return node_data;
-    };
-    this.rm = async function(){};
-  }
+    const parent_hash = root_flg ? "" : parent_node.hash;
+    const node_data = this.createModel({parent: parent_hash, key: key});
+    const create_node = await this.one.create(node_data);
+    //return await this.cd(await this.pwd(create_node)); // make後にcd
+    return create_node;
+  };
+  this.set = async function(node_data, node=null){
+    if(!node)node = this.current;
+    node_data.updated_at = APP.s_date();
+    await this.one.update(node_data);
+    return node_data;
+  };
+  this.rm = async function(){};
   return this;
 })();
--- index.js
+++ index.js
@@ -1,33 +1,22 @@
 (async conf=>{
   const DB = require('./Database');
-
   const Node = await require('./Node.js');
-  if(Node.True()){
+  if(Node){
     (async function(){
-      //初期ノード作成
-      // async function init(){
-      //   await initialize();
-      //   const NODE_DATAS = [
-      //     {hash: "NodeHash_ROOT", parent: "", key: "TestA", value: "AAA"},
-      //     {hash: "NodeHash_B", parent: "NodeHash_ROOT", key: "TestB", value: "BBB"},
-      //     {hash: "NodeHash_C", parent: "NodeHash_B", key: "TestC", value: "CCC"},
-      //     {hash: "NodeHash_D", parent: "NodeHash_B", key: "TestD", value: "DDD"},
-      //     {hash: "NodeHash_E", parent: "NodeHash_ROOT", key: "TestE", value: "EEE"},
-      //   ];
-      //   for(k in NODE_DATAS) await one.create(NODE_DATAS[k]);
-      // }
+      async function init(){
+        await initialize();
+        const root_node = await make('root', true);await checkout('root');
+        await make("work");await cd("work");
 
-      const branch1 = 'TestA/TestB/TestC/TestD';
-      const branch2 = 'TestE/TestF';
-      const branch3 = 'TestG';
+        await make("TestA/TestB/TestC/TestD"); //branch1
+        await cd("TestA/TestB");
+        await make("TestE/TestF"); //branch2
+        await make("TestG"); //branch3
+      }; await init();
 
-      await initialize();
-      const root_node = await make('root', true); await checkout('root');
+      // 各種NodeModuleテスト
+      console.log(`current: ${await pwd()}`);
       log();
-      // await make('work');await cd('work');
-      // await make(branch1);await cd('TestA/TestB');
-      // await make(branch2);
-      // await make(branch3);
     }).bind(Node)();
   }
 })({
