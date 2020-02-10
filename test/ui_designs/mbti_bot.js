b1c92e0cb5acef61a047d4549c7119668cf22a42
n-mizunuma <n-mizunuma@froide.co.jp>
2020-02-10 12:57:13 +0900

    mbti_botの概要にMTBIタイプを追記

--- test/ui_designs/mbti_bot.js
+++ test/ui_designs/mbti_bot.js
@@ -5,4 +5,9 @@
 * 各種タイプ： E/I, N/S, T/F, J/P
 * 概要：自然に生じる差異の価値を強調する事を目的としており
 * 　自分の経験を解釈する方法に特定の選好を持っており、ニーズ・価値観・動機づけの根底である
+* [TypeIndicators]
+* FOCUS(エネルギーの方向): Introvert(内向) / Extrovert(外向)
+* INFORMATION(ものの見方): iNtuitive(直感) / Sensing(感覚)
+* DECISION(判断の仕方): Thinking(思考) / Feeling(感情)
+* RESPONSE(外界への接し方): Judging(判断的態度) / Perceiving(知覚的態度)
 */
