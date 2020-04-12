59b30f5214eda7ff0208bb1ba4b4561d64a76b37
n-mizunuma <n-mizunuma@froide.co.jp>
2020-04-12 14:50:08 +0900

    検証用のvideo_streamをファイルパスが指定出来るよう修正

--- test/system/video_stream/server.js
+++ test/system/video_stream/server.js
@@ -2,15 +2,28 @@
  * Inspired by: http://stackoverflow.com/questions/4360060/video-streaming-with-html-5-via-node-js
  */
 
-var http = require('http'),
-    fs = require('fs'),
-    util = require('util');
+const http = require('http');
+const fs = require('fs');
+const util = require('util');
+
+
+const root_path = process.argv[2] ? process.argv[2] : './';
+const not_found_res = (req,res)=>{
+  res.writeHead(200,{'content-type':'text/html'});
+  res.end(`<h6>${req.url} not found!</h6>`);
+}
 
 http.createServer(function (req, res) {
-  var path = './SyntheticSympathy_Pgpf8FvySuc_240p.mp4';
-  var stat = fs.statSync(path);
-  var total = stat.size;
-  if (req.headers['range']) {
+  var path, stat, total;
+  var video_path;
+
+  const branch = req.url.slice(1).split('/');
+  if (req.headers['range']) { // streamer
+    video_path = branch.slice(1).join('/');
+    path = `${root_path}${video_path}`;
+    stat = fs.statSync(path);
+    total = stat.size;
+
     var range = req.headers.range;
     var parts = range.replace(/bytes=/, "").split("-");
     var partialstart = parts[0];
@@ -24,10 +37,23 @@ http.createServer(function (req, res) {
     var file = fs.createReadStream(path, {start: start, end: end});
     res.writeHead(206, { 'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
     file.pipe(res);
+
   } else {
-    console.log('ALL: ' + total);
-    res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'video/mp4' });
-    fs.createReadStream(path).pipe(res);
+    if(branch[0]=='_video_'){ // open
+      if(video_path=='') not_found_res(req, res);
+      res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'video/mp4' });
+      fs.createReadStream(path).pipe(res);
+
+    }else if(req.url=='/'){ // find and listup
+      fs.readFile('./index.html','utf-8',function(err,data){
+        if(!err) console.log(err);
+        res.writeHead(200, {'content-type': 'text/html'});
+        res.write(data);
+        res.end();
+      });
+
+    }else{ not_found_res(req, res); }
+
   }
 }).listen(1337, 'localhost');
 console.log('Server running at http://localhost:1337/');
