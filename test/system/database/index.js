3af3ea7bc44e815693a546093760a11218111b03
takizawa0 <seresonn_no1@yahoo.co.jp>
2020-11-15 20:16:04 +0900

    ブラウザキャプチャーとデータベース検証

--- test/system/database/index.js
+++ test/system/database/index.js
@@ -1,1 +1,18 @@
-console.log(1+1);
+const fs = require('fs');
+
+fs.writeFile('test.txt','Hello!',()=>{});
+const src = fs.createReadStream('test.txt','utf8');
+src.pipe(process.stdout);
+//const dest = fs.createWriteStream('dest.txt','utf8');
+//src.pipe(dest);
+
+// -----
+const Writable = require('stream').Writable;
+class MyWritable extends Writable{
+  constructor(options){super(options);}
+  _write(chunk,encoding,callback){
+    console.log(''+chunk);
+  }
+}
+
+src.pipe( new MyWritable('dest.txt','utf8',function(){}) );
