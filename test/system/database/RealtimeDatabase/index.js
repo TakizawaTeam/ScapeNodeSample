fbc5740b6c9261be653fc8fed94540a28139e5c0
naoto-mizunuma <n-mizunuma@froide.co.jp>
2021-04-30 00:52:19 +0900

    RealtimeDatabaseの実装方法を検証していく

--- test/system/database/RealtimeDatabase/index.js
+++ test/system/database/RealtimeDatabase/index.js
@@ -1,24 +1,45 @@
 const http = require('http');
 const ws = require('ws');
 
-let dictionary = '';
-// const 分解 = ()=>{};
-// const 構築 = ()=>{};
+let obj = {};
+let 辞書 = '';
+const rnd = n=>Math.floor(Math.random() * n);
+const 分解 = m=>{
+  if(m.length!=0){
+    lines = 辞書.split("\n");
+    m.split("").reduce((pre,curr)=>{
+      line = `${pre} ${curr}`;
+      if(lines.indexOf(line) == -1){
+        辞書 += `${line}\n`;
+        obj[pre] = curr;
+      }
+      return curr;
+    });
+  }
+};
+const 結合 = ()=>{
+  res = '';
+  keys = Object.keys(obj);
+  if(keys.length!=0){
+    key = keys[rnd(keys.length)];
+    obj_copy = { ...obj }
+    while(true){
+      if(!key){break;}else{res += key;}
+      _key = obj_copy[key];
+      delete obj_copy[key];
+      key = _key;
+    }
+    return res;
+  }else{return '';}
+};
 
 const server = http.createServer(function(req, res){});
 server.listen(5002, function(){});
 new ws.Server({server: server}).on('connection', function(wso, req){
   wso.on('message', async function(m){
-    keys = dictionary.split("\n");
-    if(m.length!=0){
-      m.split("").reduce((pre,curr)=>{
-        key = `${pre} ${curr}`;
-        if(keys.indexOf(key) == -1) dictionary += `${key}\n`;
-        return curr;
-      });
-    }
-    console.log(dictionary);
-    wso.send(`再送信：${m}`);
+    console.log(辞書);
+    分解(m);
+    wso.send(`返事：${結合()}`);
   });
   wso.on('open', async function(){
     console.log('open ws!');
